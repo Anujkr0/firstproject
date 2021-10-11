239ba937cd00270be1fcf8932a8c2ab81501111f
Anujkr0 <92168995+Anujkr0@users.noreply.github.com>
2021-10-11 20:43:33 +0530

    Add files via upload

---
+++ area.java
@@ -0,0 +1,12 @@
+class Area
+{
+   public static void main(String args[]) 
+    {  
+        
+       double r =9;
+         double  area=(22*r*r)/7 ;
+         System.out.println("Area of Circle is: " + area);      
+   }
+}
+    
+
